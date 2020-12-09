b63f876045aa5017ded71d792e77fc68446b7d2a
Nega García <nega_gl@outlook.com>
2020-12-09 13:25:53 -0500

    Proyecto model y clase estudiante creados

---
+++ src/main/java/com/clearminds/ngl/dtos/Estudiante.java
@@ -0,0 +1,51 @@
+package com.clearminds.ngl.dtos;
+
+public class Estudiante {
+	private String nombre, apellido;
+	private int id;
+
+	public Estudiante(String nombre, String apellido) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	public Estudiante(String nombre, String apellido, int id) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+	}
+	
+	
+
+}
