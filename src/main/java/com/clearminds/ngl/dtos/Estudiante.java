bdad153122b3e3ff8651fdadd963c420e93b8d4e
Nega García <nega_gl@outlook.com>
2020-12-10 10:50:20 -0500

    Se agregó atributo edad a la clase estudiantes

--- src/main/java/com/clearminds/ngl/dtos/Estudiante.java
+++ src/main/java/com/clearminds/ngl/dtos/Estudiante.java
@@ -2,24 +2,34 @@ package com.clearminds.ngl.dtos;
 
 public class Estudiante {
 	private String nombre, apellido;
-	private int id;
+	private int id, edad;
 
 	
 	public Estudiante() {
 		super();
 	}
 
-	public Estudiante(String nombre, String apellido) {
+	public Estudiante(String nombre, String apellido, int edad) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.edad = edad;
 	}
 
-	public Estudiante(String nombre, String apellido, int id) {
+	public Estudiante(int id, String nombre, String apellido) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.id = id;
+	}
+	
+
+	public Estudiante(int id, String nombre, String apellido, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.id = id;
+		this.edad = edad;
 	}
 
 	public String getNombre() {
@@ -38,6 +48,14 @@ public class Estudiante {
 		this.apellido = apellido;
 	}
 
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -48,8 +66,10 @@ public class Estudiante {
 
 	@Override
 	public String toString() {
-		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + ", edad=" + edad + "]";
 	}
+
+	
 	
 	
 
